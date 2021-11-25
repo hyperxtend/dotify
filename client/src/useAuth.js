908619590a3f8cf31240c6c534442327da9d678c
Alex Joshua <ialexjoshua@gmail.com>
2021-11-25 20:59:55 +0200

    update uris

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -10,7 +10,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post(`http://localhost:3001/login`, {
+      .post(`https://dotify-hyperxtend.herokuapp.com/login`, {
         code,
       })
       .then(res => {
@@ -28,7 +28,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post(`http://localhost:3001/refresh`, {
+        .post(`https://dotify-hyperxtend.herokuapp.com/refresh`, {
           refreshToken,
         })
         .then(res => {
